6827ab6fc9b1fd2ac4c8bd1f2980046e22b517e8
LeandroVeloso <leandrovelosorodrigues@gmail.com>
2013-05-15 19:58:07 -0300

    Adicionando classe de constantes do esporte esgrimista

---
+++ ControleAtleta/src/controleatleta/ConstantesEsgrimista.java
@@ -0,0 +1,24 @@
+
+package controleatleta;
+
+public class ConstantesEsgrimista {
+    
+    public static final byte SEXO_MASCULINO_INDICE = 0;
+    public static final byte SEXO_FEMININO_INDICE = 1;
+    public static final char SEXO_MASCULINO_VALOR = 'M';
+    public static final char SEXO_FEMININO_VALOR = 'F';
+    public static final byte ARMA_FLORETE_INDICE = 0;
+    public static final byte ARMA_ESPADA_INDICE = 1;
+    public static final byte ARMA_SABRE_INDICE = 2;
+    public static final char ARMA_FLORETE_VALOR = 'F';
+    public static final char ARMA_ESPADA_VALOR = 'E';
+    public static final char ARMA_SABRE_VALOR = 'S';
+    public static final byte CATEGORIA_ELETRICA_INDICE = 0;
+    public static final byte CATEGORIA_MUDO_INDICE = 1;
+    public static final char CATEGORIA_ELETRICA_VALOR = 'E';
+    public static final char CATEGORIA_MUDO_VALOR = 'M';
+    public static final byte ESTILO_DESTRO_INDICE = 0;
+    public static final byte ESTILO_CANHOTO_INDICE = 1;
+    public static final char ESTILO_DESTRO_VALOR = 'D';
+    public static final char ESTILO_CANHOTO_VALOR = 'C';
+}
